96f666388500099191089a22583744b021dd305f
Joef <joe.fahrenkrug@gmail.com>
2014-04-18 00:15:37 -0500

    Broken migration

--- db/migrate/20140301173306_update_conversions_again.rb
+++
@@ -1,7 +0,0 @@
-class UpdateConversionsAgain < ActiveRecord::Migration
-  def change
-    remove_column :conversions, :quanity
-    remove_column :conversions, :quantity
-    add_column :conversions, :quantity, :decimal, :precision => 8, :scale => 2 
-  end
-end
